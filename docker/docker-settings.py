8614b026183a996fa328a49d112e924f74f60453
Francisco Javier Rosa Rodríguez <frarosrod2@alum.us.es>
2020-11-30 23:50:43 +0100

    Update docker-settings.py

--- docker/docker-settings.py
+++ docker/docker-settings.py
@@ -5,8 +5,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
-	'HOST': 'db',
+        'PASSWORD':'postgres',
+        'HOST': 'db',
         'PORT': 5432,
     }
 }
